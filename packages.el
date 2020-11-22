509d1201b95f65aaeb172c8482f36605b2291326
Zarak <7254237+zarak@users.noreply.github.com>
2020-11-22 23:15:48 +0500

    Add doom config

--- packages.el
+++ packages.el
@@ -1,9 +1,63 @@
 ;; -*- no-byte-compile: t; -*-
-;;; config/default/packages.el
+;;; $DOOMDIR/packages.el
 
-(package! avy :pin "509471bad0e8094b8639729ec39ca141fae7d4bd")
-(package! drag-stuff :pin "6d06d846cd37c052d79acd0f372c13006aa7e7c8")
-(package! link-hint :pin "7440704cacb5c0fab35fff8ec59d30fbea17f44a")
+;; To install a package with Doom you must declare them here and run 'doom sync'
+;; on the command line, then restart Emacs for the changes to take effect -- or
+;; use 'M-x doom/reload'.
 
-(unless (featurep! :editor evil)
-  (package! expand-region :pin "ea6b4cbb9985ddae532bd2faf9bb00570c9f2781"))
+
+;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
+;(package! some-package)
+
+;; To install a package directly from a remote git repo, you must specify a
+;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
+;; https://github.com/raxod502/straight.el#the-recipe-format
+;(package! another-package
+;  :recipe (:host github :repo "username/repo"))
+(package! company-org-roam
+   :recipe (:host github :repo "jethrokuan/company-org-roam"))
+
+(package! lean-mode
+   :recipe (:host github :repo "leanprover/lean-mode"))
+
+(package! company-lean)
+(package! helm-lean)
+(package! org-drill)
+(package! org-super-agenda)
+(package! haskell-mode)
+(package! anki-editor)
+(package! org-download)
+
+;; If the package you are trying to install does not contain a PACKAGENAME.el
+;; file, or is located in a subdirectory of the repo, you'll need to specify
+;; `:files' in the `:recipe':
+;(package! this-package
+;  :recipe (:host github :repo "username/repo"
+;           :files ("some-file.el" "src/lisp/*.el")))
+
+;; If you'd like to disable a package included with Doom, you can do so here
+;; with the `:disable' property:
+;(package! builtin-package :disable t)
+
+;; You can override the recipe of a built in package without having to specify
+;; all the properties for `:recipe'. These will inherit the rest of its recipe
+;; from Doom or MELPA/ELPA/Emacsmirror:
+;(package! builtin-package :recipe (:nonrecursive t))
+;(package! builtin-package-2 :recipe (:repo "myfork/package"))
+
+;; Specify a `:branch' to install a package from a particular branch or tag.
+;; This is required for some packages whose default branch isn't 'master' (which
+;; our package manager can't deal with; see raxod502/straight.el#279)
+;(package! builtin-package :recipe (:branch "develop"))
+
+;; Use `:pin' to specify a particular commit to install.
+;(package! builtin-package :pin "1a2b3c4d5e")
+
+
+;; Doom's packages are pinned to a specific commit and updated from release to
+;; release. The `unpin!' macro allows you to unpin single packages...
+;(unpin! pinned-package)
+;; ...or multiple packages
+;(unpin! pinned-package another-pinned-package)
+;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
+;(unpin! t)
