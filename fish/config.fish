113db5b8b25c1502036b94f1ac7cd64c9cd5411b
Zarak <7254237+zarak@users.noreply.github.com>
2021-07-06 07:14:55 +0500

    Add fish config

---
+++ fish/config.fish
@@ -0,0 +1,29 @@
+starship init fish | source
+
+# Disable greeting
+# set fish_greeting
+
+function fish_greeting
+  command neofetch
+end
+
+fish_vi_key_bindings
+# fish_default_key_bindings
+
+# doom emacs
+# set PATH $PATH:~/.emacs.d/bin
+fish_add_path ~/.emacs.d/bin
+
+# Pet
+function prev
+  set line (echo $history[1])
+  pet new $line
+end
+
+if type rg &> /dev/null
+  export FZF_DEFAULT_COMMAND='rg --files'
+  export FZF_DEFAULT_OPTS='-m --height 50% --border'
+end
+
+# set fish_ambiguous_width 1
+# set fish_emoji_width 1
